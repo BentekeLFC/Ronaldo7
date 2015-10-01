80fff7b5bb02f33e26c1a654850a37e7ea33fe1b
BentekeLFC <finaldaniel898@hotmail.com>
2015-10-01 16:11:21 +0800

    Update Myclient.c

--- Myclient.c
+++ Myclient.c
@@ -8,25 +8,16 @@ GROUP MEMBERS:
 2) HABINASH BALAKRISHAN	        1122700044 
 */
 
-
 #include "inet.h"
-#define PORT 8000
-#define SERVER_IP "127.0.0.1"
-#define MAXSZ 1024
-int main()
-{
+#define BUFSIZE 1024
+
+main(int argc, char *argv[]){
 int sockfd;
-//to create the socket
-
-struct sockaddr_in serverAddress;
-//The client will connect to this
-
-int n;
-int bfreceiv = 0;
-char mmo[MAXSZ+1];
-char cr8[25];
-char dlt[25];
-
+char buffer[BUFSIZE+1];
+int bytereceive = 0;
+struct sockaddr_in serv_addr;
+char createname[20];
+char deletename[20];
 static struct sigaction act;
 
 void catchin(int);
@@ -37,57 +28,88 @@ sigfillset(&(act.sa_mask));
 sigaction(SIGINT, &act, (void *) 0);
 
 
-//creates the socket
-sockfd=socket(AF_INET,SOCK_STREAM,0);
-//initializes the socket addresses
-memset(&serverAddress,0,sizeof(serverAddress));
-serverAddress.sin_family=AF_INET;
-serverAddress.sin_addr.s_addr=inet_addr(SERVER_IP);
-serverAddress.sin_port=htons(PORT);
+if(argc <= 1){
+printf("How to use : %s remoteIPaddress [example: ./client 127.0.0.1]\n", argv[0]); exit(1); }
 
-//client  connecting to the server
-connect(sockfd,(struct sockaddr *)&serverAddress,sizeof(serverAddress));
+bzero( (char*) &serv_addr, sizeof(serv_addr) );
+serv_addr.sin_family = AF_INET ;
+serv_addr.sin_port = htons (SERV_TCP_PORT);
+inet_pton (AF_INET, argv[1], &serv_addr.sin_addr);
 
-/* Gets the username */ 
-	char *usr; 
-	usr=(char *)malloc(10*sizeof(char)); 
-	usr=getlogin(); 
+if( (sockfd = socket(AF_INET, SOCK_STREAM, 0) ) < 0) {
+perror("Client: socket() error \n");
+exit(1); }
 
+if(connect (sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) <0 ) {
+perror("Client: connect() error\n");
+exit(1);}
 
+/* Get the user name */ 
+	char *buf; 
+	buf=(char *)malloc(10*sizeof(char)); 
+	buf=getlogin(); 
+
+	/* set the 'client_file' path */ 
 	char str[30]; 
 	strcpy(str, "/home/"); 
-	strcat(str, usr); 
-	strcat(str, "/clientFile/"); 
-/* Setting the 'clientFile' path */ 
+	strcat(str, buf); 
+	strcat(str, "/client_file/"); 
 
 	/* Check the path exist or not, if not, create one */ 
 	struct stat s; 
 	if(stat(str, &s) == -1){ 
 	mkdir(str, 0700); }
 
-//send to sever and receive from server
-do
+do{
+bzero( buffer, sizeof(buffer));
+recv(sockfd, buffer, BUFSIZE, 0);
+printf("\n%s\n", buffer); 
+gets(buffer);
+send(sockfd,buffer, BUFSIZE, 0);
+
+
+if(!strcmp(buffer, "1"))
 {
-bzero( mmo, sizeof(mmo));
-recv(sockfd,mmo,MAXSZ,0);
+	bzero( buffer, sizeof(buffer));
+	recv(sockfd, buffer, BUFSIZE, 0);
+	printf("\n%s\n", buffer); 
+	gets(buffer);
+	send(sockfd,buffer, BUFSIZE, 0);
+	
+   	char filename[30];
+    	strcpy(filename, "/home/"); 
+    	strcat(filename, buf); 
+    	strcat(filename, "/client_file/");
+    	strcat(filename, buffer);
+    
+    	FILE *fp;
+    	fp = fopen(filename, "ab"); 
 
-printf("%s\n",mmo);
+		if(NULL == fp)
+    	{
+        	printf("Error opening file");
+        
+    	}
+    
+    	bzero( buffer, sizeof(buffer));
+    
+    	bytereceive = recv(sockfd, buffer, BUFSIZE, 0);
+    	fwrite(buffer,1,bytereceive,fp);
+   
+}
 
 
-gets(mmo);
-
-send(sockfd,mmo,MAXSZ,0);
-
- if(!strcmp(mmo, "1"))
+else if(!strcmp(buffer, "2"))
 {
 
+		
 	DIR *dir;
 	struct dirent *ent;
 
 	char directoryName[30];
    	strcpy(directoryName, "/home/"); 
-    	strcat(directoryName, usr); 
-    	strcat(directoryName, "/clientFile/");
+    	strcat(directoryName, buf); 
+    	strcat(directoryName, "/client_file/");
 
 	if ((dir = opendir (directoryName)) != NULL) {
 		
@@ -100,122 +122,79 @@ send(sockfd,mmo,MAXSZ,0);
  		closedir (dir);
 	}
 
-	printf("\nKindly enter one of the filename from above including extension\n");
+	printf("\nPlease enter one of the filename from above including extension\n");
 	
-	bzero( mmo, sizeof(mmo));
-	
-	gets(mmo);
-	send(sockfd,mmo, MAXSZ, 0);
+	bzero( buffer, sizeof(buffer));
+	gets(buffer);
+	send(sockfd,buffer, BUFSIZE, 0);
 
-	char NamaFile[40];
-	strcpy(NamaFile, "/home/"); 
-	strcat(NamaFile, usr); 
-	strcat(NamaFile, "/serverFile/");
+	char filename[30];
+	strcpy(filename, "/home/"); 
+	strcat(filename, buf); 
+	strcat(filename, "/client_file/");
 	    
-	strcat(NamaFile, mmo);
+	strcat(filename, buffer);
 
 	FILE *fp;
-    	fp = fopen(NamaFile, "r"); 
+    	fp = fopen(filename, "r"); 
 
-	bzero( mmo, sizeof(mmo));
-	int nread = fread(mmo,1,256,fp);
-    	bfreceiv = recv(sockfd, mmo, MAXSZ, 0);
-    	fwrite(mmo,1,bfreceiv,fp);
-
+	bzero( buffer, sizeof(buffer));
+	int nread = fread(buffer,1,256,fp);
+	send(sockfd, buffer, nread, 0);	
 }
 
 
-else if(!strcmp(mmo, "2"))
+else if(!strcmp(buffer, "3"))
 {
-	
-printf("Write down a filename to be downloaded to the [clientFile]\n\n" );
-
-	//gets(mmo);
-	//send(sockfd,mmo, MAXSZ, 0);
-	bzero( mmo, sizeof(buffer));
-	recv(sockfd, mmo, MAXSZ, 0);
-	printf("\n%s\n", mmo); 
-	gets(mmo);
-	send(sockfd,mmo, MAXSZ, 0);
-	
-   	char NamaFile[40];
-    	strcpy(NamaFile, "/home/"); 
-    	strcat(NamaFile, usr); 
-    	strcat(NamaFile, "/clientFile/");
-    	strcat(NamaFile, mmo);
-    
-    	FILE *fp;
-    	fp = fopen(NamaFile, "ab"); 
-
-		if(NULL == fp)
-    	{
-        	printf("Error opening file");
-        
-    	}
-    
-    	bzero( mmo, sizeof(mmo));
-    
-    	bfreceiv = recv(sockfd, mmo, MAXSZ, 0);
-    	fwrite(mmo,1,bfreceiv,fp);
-   
-}
-
-
-
-else if(!strcmp(mmo, "3"))
-{
-	printf("Please key in the Directory that attempt to create: ");
-	scanf("%s", cr8);
+	printf("Enter directory name that you want to create: ");
+	scanf("%s", createname);
 
 	/* set the path/name of the directory that want to create */ 
-	char cr8Direc[40]; 
-	strcpy(cr8Direc, "/home/"); 
-	strcat(cr8Direc, usr); 
-	strcat(cr8Direc, "/"); 
-	strcat(cr8Direc, cr8);
+	char createDirectory[30]; 
+	strcpy(createDirectory, "/home/"); 
+	strcat(createDirectory, buf); 
+	strcat(createDirectory, "/"); 
+	strcat(createDirectory, createname);
 
 	/* Check the path exist or not, if not, create one */ 
 	struct stat s; 
-	if(stat(cr8Direc, &s) == -1){ 
-	mkdir(cr8Direc, 0700); } 
+	if(stat(createDirectory, &s) == -1){ 
+	mkdir(createDirectory, 0700); } 
 }
 
 
-else if(!strcmp(mmo, "4"))
+else if(!strcmp(buffer, "4"))
 {
-	printf("Please key in the specified Directory to remove it: ");
-	scanf("%s", dlt);
+	printf("Enter directory name that you want to delete: ");
+	scanf("%s", deletename);
 
 	/* set the path of the directory that want to delete */ 
-	char dltDirec[40]; 
-	strcpy(dltDirec, "/home/"); 
-	strcat(dltDirec, usr); 
-	strcat(dltDirec, "/"); 
-	strcat(dltDirec, dlt);
+	char deleteDirectory[30]; 
+	strcpy(deleteDirectory, "/home/"); 
+	strcat(deleteDirectory, buf); 
+	strcat(deleteDirectory, "/"); 
+	strcat(deleteDirectory, deletename);
 
-	
-	char slctSubDirec[60];
-	strcpy(slctSubDirec, "exec rm -r ");
-	strcat(slctSubDirec, "/home/"); 
-	strcat(slctSubDirec, usr); 
-	strcat(slctSubDirec, "/"); 
-	strcat(slctSubDirec, dlt);
-	strcat(slctSubDirec, "/*"); 
-        /* select all the files inside the directory that want to remove */
-	
+	/* select all the files inside the directory that want to delete */
+	char selectSubDirectory[50];
+	strcpy(selectSubDirectory, "exec rm -r ");
+	strcat(selectSubDirectory, "/home/"); 
+	strcat(selectSubDirectory, buf); 
+	strcat(selectSubDirectory, "/"); 
+	strcat(selectSubDirectory, deletename);
+	strcat(selectSubDirectory, "/*"); 
+
+	/* Check the path exist or not, if exist, delete it */ 
 	struct stat s; 
-	if(stat(dltDirec, &s) != -1){
-	system(slctSubDirec);
-	rmdir(dltDirec); } 
-        /* Checking that the specified path exist or not, if exist, delete it */ 
+	if(stat(deleteDirectory, &s) != -1){
+	system(selectSubDirectory);
+	rmdir(deleteDirectory); } 
 }
 
-}while (strcmp(mmo, "/q"));
+}while (strcmp(buffer, "/q"));
 close(sockfd);
-return 0;
 }
 
 void catchin(int signo){
-	printf("\n[ Interruption found in the signal.....]\n");
+	printf("\n[ Interrupt signal has been ignored.]\n");
 }
-
